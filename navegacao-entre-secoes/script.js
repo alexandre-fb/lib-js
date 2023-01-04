a086fd06e40a828acba87c1c228d6fc5d70df042
alexandrefb <alexandrefb.86@gmail.com>
2023-01-04 16:14:43 -0300

    Add active current link

--- navegacao-entre-secoes/script.js
+++ navegacao-entre-secoes/script.js
@@ -1,8 +1,9 @@
+//=====Animar seções
 //Pega os ementos que serão animados
 const elementsToAnimate = document.querySelectorAll('.scroll-animation') 
 
 //Pega % da altura da tela
-const screenHeightPercentage = innerHeight * 0.7
+const screenHeightPercentage = innerHeight * 0.5
 
 //Função de animação
 function animateToScroll () {
@@ -25,8 +26,33 @@ function animateToScroll () {
     })) 
 }
 
+//=====Deixar ativo o link atual
+function activeLink() {
+    //Pega as seções
+    const sections = document.querySelectorAll('section[id]')
+
+    //faz o loop por cada seção
+    sections.forEach((section, index) => {
+        const sectionTop = section.getBoundingClientRect().top
+        const sectionBottom = section.getBoundingClientRect().bottom
+        const positionToActive = sectionTop - screenHeightPercentage
+        const positionToInactive = sectionBottom - screenHeightPercentage
+        const sectionId = section.getAttribute('id')
+
+        if(positionToActive < 0 && positionToInactive > 0) {
+            const currentSection = document.querySelector('li a[href*=' + sectionId + ']')
+            currentSection.classList.add('active')
+        } else {
+            const currentSection = document.querySelector('li a[href*=' + sectionId + ']')
+            currentSection.classList.remove('active')
+        }
+    })
+  }
+
 window.addEventListener('scroll', () => {
     animateToScroll()
+    activeLink()
 })
 
 animateToScroll()
+activeLink()
