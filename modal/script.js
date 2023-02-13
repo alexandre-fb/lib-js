bb5edb59fae454406724f8270191a7445f4cdcbf
alexandrefb <alexandrefb.86@gmail.com>
2023-02-13 12:30:17 -0300

    Refactor

--- modal/script.js
+++ modal/script.js
@@ -2,21 +2,25 @@ const buttonOpen = document.querySelector('[data-modal="open"]')
 const buttonClose  = document.querySelector('[data-modal="close"]')
 const containerModal = document.querySelector('[data-modal="container"]')
 
-function openModal() {
-    containerModal.classList.add('active')
-}
-
-function closeModal() {
-    containerModal.classList.remove('active')
-}
-
-function clickOutsideModal(event) {
-    if(event.target === this) {
-        closeModal()
-        //O this é o elemento que vai receber o evento de click, nesse caso o containerModal.
+function initModal() {
+    if (buttonOpen && buttonClose && containerModal) {
+    //Validação para não travar o js todo caso não existam os elementos.
+        
+        function toggleModal() {
+            containerModal.classList.toggle('active')
+        }
+        
+        function clickOutsideModal(event) {
+            if(event.target === this) {
+                toggleModal()
+                //O this é o elemento que vai receber o evento de click, nesse caso o containerModal.
+            }
+        }
+        
+        buttonOpen.addEventListener('click', toggleModal)
+        buttonClose.addEventListener('click', toggleModal)
+        containerModal.addEventListener('click', clickOutsideModal)
     }
 }
 
-buttonOpen.addEventListener('click', openModal)
-buttonClose.addEventListener('click', closeModal)
-containerModal.addEventListener('click', clickOutsideModal)
+initModal()
