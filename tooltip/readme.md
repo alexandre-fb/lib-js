f088aee5e61e4c1e72d017d9fcbaa14e5e3a3bad
alexandrefb <alexandrefb.86@gmail.com>
2023-02-15 16:15:04 -0300

    Refactor

--- tooltip/readme.md
+++ tooltip/readme.md
@@ -2,5 +2,5 @@
     <a href="https://alexandre-fb.github.io/lib-js/tooltip/index.html" target="_blank">🔗 Acessar página</a>
 </p>
 <p align="center" width="100%">
-    <img class="" src="../assets/gifs/modal.gif" alt="navegação por tab" width="400px" style="margin: 0 auto;">
+    <img class="" src="../assets/gifs/tooltip.gif" alt="tooltip" width="400px" style="margin: 0 auto;">
 </p>
--- tooltip/script.js
+++ tooltip/script.js
@@ -6,6 +6,7 @@ function initTooltip() {
     //Validação para não travar o js todo caso não encontre o elemento.
 
         function createTootip(element) {
+            //cria o tooltip e coloca dentro do body
             const tooltipBox = document.createElement('div')
             const tootltipText = element.getAttribute('aria-label')
             tooltipBox.classList.add('tooltip')
@@ -16,10 +17,13 @@ function initTooltip() {
 
         function handleMouseEnter() {
             const tooltip = createTootip(this)
+            //como o retorno da função é o tooltip consigo colocar ele em uma variável
+            //o this é o elemento que recebe o evento usando essa função
 
             this.addEventListener('mousemove', (event) => {
-                tooltip.style.top = event.pageY -50 + 'px'
-                tooltip.style.left = event.pageX -25 + 'px'
+                tooltip.style.top = event.pageY - 50 + 'px'
+                tooltip.style.left = event.pageX - 25 + 'px'
+                //o -50 e -25 é para mover um pouco pra longe do cursor.
             })  
             
             this.addEventListener('mouseleave', () => {
